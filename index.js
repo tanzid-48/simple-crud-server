9d5fe89975dc1025e444d1480b36b85bdce9c819
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-07 00:40:22 +0600

    Query MongoDB to get user data

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port =  process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 // Adds headers: Access-Control-Allow-Origin: *
-app.use(cors())
+app.use(cors());
 // Json
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhm3y2q.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,30 +19,40 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const run = async() => {
-     try {
+const run = async () => {
+  try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Get the database and collection on which to run the operation
+    const db = client.db("simpleCrud");
+    const userCollection = db.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
-}
+};
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Simple Crud server Serving')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Simple Crud server Serving");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
