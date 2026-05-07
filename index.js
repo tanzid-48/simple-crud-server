2b1c132863146d778e6e7994f0217e8835b7dd6e
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-07 15:00:10 +0600

    Update route on server

--- index.js
+++ index.js
@@ -54,7 +54,26 @@ const run = async () => {
       const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
       res.send(result);
+    });
+
+    
+    app.patch('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {
+        _id: new ObjectId(id)
+      }
+      const modifiedUser = req.body;
+      const updatedDocument = {
+        $set:{
+          name:modifiedUser.name,
+          email:modifiedUser.email,
+          role:modifiedUser.role
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDocument);
+      res.send(result);
     })
+
 
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
